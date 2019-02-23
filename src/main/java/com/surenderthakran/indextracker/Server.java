405b82b3fb34161e0524bbb1ca5f0fd81bb4baf8
surenderthakran <surender.thakran89@gmail.com>
2019-02-23 17:50:14 +0530

    Updated to use flogger

--- src/main/java/com/surenderthakran/indextracker/Server.java
+++ src/main/java/com/surenderthakran/indextracker/Server.java
@@ -1,7 +1,11 @@
 package com.surenderthakran.indextracker;
 
+import com.google.common.flogger.FluentLogger;
+
 public class Server {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    logger.atInfo().log("Hello World!");
   }
 }
