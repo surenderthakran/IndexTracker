b0c669b916c0d6dc6aecaeacca922aab28560861
surenderthakran <surender.thakran89@gmail.com>
2019-03-22 17:22:36 +0530

    Cleaned up GetStockHandler

--- src/main/java/com/surenderthakran/indextracker/handlers/getstock/GetStockHandler.java
+++ src/main/java/com/surenderthakran/indextracker/handlers/getstock/GetStockHandler.java
@@ -1,32 +1,19 @@
 package com.surenderthakran.indextracker.handlers.getstock;
 
 import com.google.common.flogger.FluentLogger;
-import com.google.common.io.CharStreams;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.sun.net.httpserver.HttpExchange;
 import com.surenderthakran.indextracker.net.Handler;
 import com.surenderthakran.indextracker.net.Request;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 public class GetStockHandler implements Handler {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   @Override
-  public JsonElement handle(HttpExchange exchange, Request req) throws IOException {
+  public JsonElement handle(HttpExchange exchange, Request req) {
     GetStockRequest request = (GetStockRequest) req;
     logger.atInfo().log("Handling GetStock request");
-
-    System.out.println(exchange.getRequestURI());
-    System.out.println(exchange.getLocalAddress());
-    System.out.println(exchange.getRequestMethod());
-
-    InputStream inputStream = exchange.getRequestBody();
-    String body = CharStreams.toString(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-    System.out.println(body);
 
     Gson gson = new Gson();
 
--- src/main/java/com/surenderthakran/indextracker/net/Handler.java
+++ src/main/java/com/surenderthakran/indextracker/net/Handler.java
@@ -2,8 +2,7 @@ package com.surenderthakran.indextracker.net;
 
 import com.google.gson.JsonElement;
 import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
 
 public interface Handler {
-  public JsonElement handle(HttpExchange exchange, Request request) throws IOException;
+  public JsonElement handle(HttpExchange exchange, Request request);
 }
