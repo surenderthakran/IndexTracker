a596a34ac751017245c7cbb0572609d4623607da
surenderthakran <surender.thakran89@gmail.com>
2019-03-24 19:50:22 +0530

    Started adding test file for Router.java

---
+++ src/test/java/com/surenderthakran/indextracker/net/RouterTest.java
@@ -0,0 +1,12 @@
+package com.surenderthakran.indextracker.net;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RouterTest {
+  @Test
+  public void test() {
+    assertEquals(6, 6);
+  }
+}
